1024904f7226e319b359db9e784bef5f9748e43f
Derrick Weis <derrick@derrickweis.com>
2025-02-22 21:41:28 -0800

    disable format lsp fallback fallback for c,cpp,odin

--- config/plugins/conform.nix
+++ config/plugins/conform.nix
@@ -2,7 +2,20 @@
   plugins.conform-nvim = {
     enable = true;
     settings = {
-      format_on_save.lsp_format = "fallback";
+      format_on_save = ''
+            function(bufnr)
+              local disable_filetypes = { c = true, cpp = true, odin = true }
+              return {
+                timeout_ms = 250,
+                lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype]
+              }
+            end
+        '';
+
+      formatters_by_ft = {
+        "rust" = ["rustfmt"];
+        " " = ["trim_whitespace"];
+      };
     };
   };
 }
